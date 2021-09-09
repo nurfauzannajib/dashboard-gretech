2706477435c1c18e51f5e12face30c318ca0e0fa
Nur Fauzan Najib <nurfauzannajib@gmail.com>
2021-09-09 14:14:06 +0700

    Update Summary.js

--- src/partials/dashboard/Summary.js
+++ src/partials/dashboard/Summary.js
@@ -5,8 +5,10 @@ function Summary() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-6 bg-white shadow-lg rounded-sm border border-gray-200 -mt-6">
       <header className="px-8 py-4 border-b border-gray-100 flex items-center justify-between">
-        <div className="h-10 w-10 bg-gray-400 flex "></div>
-        <h2 className="absolute pl-10 font-semibold text-4xl text-gray-800 ">
+        <div className="h-16 w-16 rounded-sm shadow-lg bg-gray-400 flex">
+          <p>Average</p>
+        </div>
+        <h2 className="absolute pl-20 font-semibold text-4xl text-gray-800 ">
           Co
         </h2>
 
